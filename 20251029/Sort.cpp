874175ff2149958e2152bd3e67161dd10c79d613
Knazawa-Yuusei <yuutaku607@gmail.com>
2025-10-29 17:33:20 +0900

    配列のソートとソート後の表示の実装

--- 20251029/Sort.cpp
+++ 20251029/Sort.cpp
@@ -5,17 +5,44 @@
 int main()
 {
     srand(time(nullptr));
-    int sortNumbers[10];
 
-    for (int i = 0;i < 10;i++)
+    int sortNumbers[10];
+    int numbersIndex = 10;
+    int minNumber;
+
+    for (int i = 0;i < numbersIndex;i++)
     {
         sortNumbers[i] = rand() % 100 + 1;
     }
     
-        for (int i = 0;i < 10;i++)
+    for (int i = 0;i < numbersIndex;i++)
     {
         std::cout << sortNumbers[i];
         std::cout << "　";
     }
     std::cout << std::endl;
+
+    for (int i= 0;i < numbersIndex - 1;i++)
+    {
+        minNumber = sortNumbers[i];
+
+        for (int j = i;j < numbersIndex;j++)
+        {
+            if (minNumber > sortNumbers[j])
+            {
+                minNumber = sortNumbers[j];
+                sortNumbers[j] = sortNumbers[i];
+                sortNumbers[i] = minNumber;
+            }
+        }
+    }
+
+    for (int i = 0;i < numbersIndex;i++)
+    {
+        std::cout << sortNumbers[i];
+        std::cout << "　";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
